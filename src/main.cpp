b1d7f8bd7ddea799613314b1a0627cd11d32c107
matrunych <olenka.matrunych@gmail.com>
2020-09-10 18:20:56 +0300

    read and write without hidden symbols

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,34 @@
 //#include "operations/operations.hpp"
 #include <fcntl.h>
 
+#define BYTECOUNT 255
 
-int writebuffer(int fd, char* buffer, ssize_t size, int* status){
+
+int write_buffer(int fd, char *buffer, ssize_t size, int *status) {
     ssize_t written_bytes = 0;
-    while( written_bytes < size ) {
-        ssize_t written_now = write(fd, buffer + written_bytes, size - written_bytes );
-        if ( written_now == -1){
+    while (written_bytes < size) {
+        ssize_t written_now = write(fd, buffer + written_bytes, size - written_bytes);
+        if (written_now == -1) {
             if (errno == EINTR)
                 continue;
-            else{
+            else {
                 *status = errno;
                 return -1;
             }
-        }else
+        } else
             written_bytes += written_now;
     }
     return 0;
 }
 
-int read_buffer(int fd, char* buffer, ssize_t size, int* status){
+int read_buffer(int fd, char *buffer, ssize_t size, int *status) {
     ssize_t read_bytes = 0;
-    while (read_bytes < size){
+    while (read_bytes < size) {
         ssize_t read_now = read(fd, buffer + read_bytes, size - read_bytes);
-        if (read_now == -1){
+        if (read_now == -1) {
             if (errno == EINTR)
                 continue;
-            else{
+            else {
                 *status = errno;
                 return -1;
             }
@@ -40,41 +42,9 @@ int read_buffer(int fd, char* buffer, ssize_t size, int* status){
 }
 
 
-
 int main(int argc, char **argv) {
-//    int variable_a, variable_b;
-//
-    namespace po = boost::program_options;
-//
-//    po::options_description visible("Supported options");
-//    visible.add_options()
-//            ("help,h", "Print this help message.");
-//
-//    po::options_description hidden("Hidden options");
-//    hidden.add_options()
-//            ("a", po::value<int>(&variable_a)->default_value(0), "Variable A.")
-//            ("b", po::value<int>(&variable_b)->default_value(0), "Variable B.");
-//
-//    po::positional_options_description p;
-//    p.add("a", 1);
-////    p.add("b", 1);
-//
-//    po::options_description all("All options");
-//    all.add(visible).add(hidden);
-//
-//    po::variables_map vm;
-//    po::store(po::command_line_parser(argc, argv).options(all).positional(p).run(), vm);
-//    po::notify(vm);
-//
-//
-//
-//    if (vm.count("help")) {
-//        std::cout << "Usage:\n  add [a] [b]\n" << visible << std::endl;
-//        return EXIT_SUCCESS;
-//    }
 
-//    int result = operations::add(variable_a, variable_b);
-//    std::cout << result << std::endl;
+    namespace po = boost::program_options;
 
     std::vector<std::string> files;
     po::options_description visible("Supported options");
@@ -91,9 +61,6 @@ int main(int argc, char **argv) {
             options(visible).positional(p).run(), vm);
     po::notify(vm);
 
-//    for (auto f:files) {
-//        std::cout<<f<<std::endl;
-//    }
 
     if (vm.count("help")) {
         std::cout << "Concatenate given files and output to stdout.\n" << visible << std::endl;
@@ -101,21 +68,44 @@ int main(int argc, char **argv) {
     }
 
     bool a = vm.count("-A") != 0;
+    std::cout << a << std::endl;
+
 
     int fd;
-    std::vector <int> descriptors;
+    std::vector<int> descriptors;
     for (auto f : files) {
-        std::cout<< "sdcc" << std::endl;
-
         std::cout << f << std::endl;
         fd = open(f.c_str(), O_RDONLY);
-        if(fd == -1){
+        if (fd == -1) {
+            std::cerr << "Cannot open " << f << std::endl;
             return -1;
         }
         descriptors.push_back(fd);
     }
-    for (auto x: descriptors){
-        std::cout<< x << std::endl;
+
+
+    char *buffer;
+    buffer = (char *) malloc(BYTECOUNT + 1);
+    int status;
+    int s;
+
+    for (auto fd: descriptors) {
+        off_t size = lseek(fd, 0, SEEK_END);
+        lseek(fd, 0, SEEK_SET);
+        s = 0;
+        while (s + BYTECOUNT < size) {
+            read_buffer(fd, buffer, BYTECOUNT, &status);
+            write_buffer(1, buffer, BYTECOUNT, &status);
+            s += BYTECOUNT;
+        }
+        int extra = size - s;
+        read_buffer(fd, buffer, extra, &status);
+        write_buffer(1, buffer, extra, &status);
+
+        if (close(fd) == -1) {
+            std::cerr << "Cannot close " << std::endl;
+            return -1;
+        }
     }
 
 
